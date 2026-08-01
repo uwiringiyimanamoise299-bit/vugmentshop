d82e3b2659cf25f9676cfeccfa8818f2ddb6b99e
uwiringiyimanamoise299-bit <uwiringiyimanamoise299@gmail.com>
2026-08-01 12:27:20 +0200

    fix: change icon-sm to icon size in dialog and sheet components

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -65,8 +65,8 @@ function DialogContent({
             render={
               <Button
                 variant="ghost"
-                className="absolute top-2 right-2"
-                size="icon-sm"
+                className="absolute top-2 right-2 [&_svg]:size-4"
+                size="icon"
               />
             }
           >
